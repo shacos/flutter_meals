18a3949ba829052ea614a1872248f25f33bb52b6
Mehdi Chikhi <ci_mi@outlook.fr>
2024-05-06 20:30:36 +0100

    finished lecture 171

---
+++ lib/screens/filters.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key});
+
+  @override
+  State<FiltersScreen> createState() {
+    return _FiltersScreenState();
+  }
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _isGlutenFree = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Filters'),
+      ),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: _isGlutenFree,
+            onChanged: (isChecked) {
+              setState(() {
+                _isGlutenFree = isChecked;
+              });
+            },
+            title: Text(
+              'Gluten-free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Only include gluten-free meals.',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(right: 34, left: 22),
+          )
+        ],
+      ),
+    );
+  }
+}
